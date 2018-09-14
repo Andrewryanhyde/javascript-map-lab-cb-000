2a966ee30bd82ae5f78046aa5f14beb0e363b101
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-09-14 09:59:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,3 +21,56 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+const knownDecepticons = ['Megatron', 'Skywarp', 'Laserbeak', 'Barricade'];
+
+const robots = [
+  { name: 'Bumblebee', alliance: null },
+  { name: 'Laserbeak', alliance: null },
+  { name: 'Barricade', alliance: null },
+  { name: 'Optimus Prime', alliance: null },
+  { name: 'Skywarp', alliance: null },
+  { name: 'Megatron', alliance: null },
+  { name: 'Ironhide', alliance: null },
+  { name: 'Ratchet', alliance: null }
+];
+
+const zebraStripes = [
+  { width: 9.12, color: null },
+  { width: 5.71, color: null },
+  { width: 6.01, color: null },
+  { width: 1.54, color: null },
+  { width: 8.34, color: null },
+  { width: 7.77, color: null },
+  { width: 0.59, color: null },
+  { width: 7.31, color: null }
+];
+
+
+
+const sortedRobots = robots.map( function(robot) {
+  if (knownDecepticons.includes(robot.name)) {
+    return Object.assign({}, robot, {
+      name: robot.name,
+      alliance: "decepticon"
+    });
+  }
+})
+
+let i = 0
+
+const coloredZebraStripes = zebraStripes.map(function(zebra, i){
+  if (i%2 === 0){
+    return Object.assign({}, zebra, {
+      width: zebra.width,
+      color: "black"
+    })
+  }
+  else {
+    return Object.assign({}, zebra, {
+      width: zebra.width,
+      color: "white"
+    });
+  }
+  i++
+});
